b914fbf8dff59abe77cc125f5f54988a1d6d5dd0
RyleyGG <griffithryley2000@gmail.com>
2024-01-20 11:26:16 -0500

    code cleanup

--- backend/conftest.py
+++ backend/conftest.py
@@ -9,37 +9,6 @@ from models.db_models import User, Course
 from services.api_utility_service import get_session, dbUrl
 
 
-# @pytest.fixture(scope="module")
-# def override_db_depend(db_session):
-#     app.dependency_overrides[get_session] = lambda: db_session
-#     yield
-#     app.dependency_overrides.pop(get_session, None)
-#
-#
-# @pytest.fixture(scope="module")
-# def test_client():
-#     with TestClient(app) as client:
-#         yield client
-#
-#
-# @pytest.fixture(scope="module")
-# def db_session():
-#     engine = create_engine(dbUrl.replace('@db', '@localhost'))
-#     TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-#     connection = engine.connect()
-#     transaction = connection.begin()
-#     SQLModel.metadata.create_all(engine)
-#     db = TestingSessionLocal(bind=connection)
-#
-#     # delete all data before starting tests
-#     db.query(User).delete()
-#     db.commit()
-#
-#     yield db
-#
-#     transaction.rollback()
-#     connection.close()
-
 @pytest.fixture(name="db", scope="module")
 def session_fixture():
     engine = create_engine(dbUrl.replace('@db', '@localhost'))
