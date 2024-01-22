98a832c271f560c8c1bb7bcc2293cbf9a4ea1450
Sn00pyW00dst0ck <gabealdous@gmail.com>
2024-01-22 15:41:11 -0500

    Setup general routes and routes for login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { signinGuard, signoutGuard, signupGuard } from './auth/oauth2.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // Routes for authentication
+    { path: "sign_up", component: AppComponent, canActivate: [signupGuard] },
+    { path: "sign_in", component: AppComponent, canActivate: [signinGuard] },
+    { path: "sign_out", component: AppComponent, canActivate: [signoutGuard] },
+
+    // General routes
+    { path: "landing", component: AppComponent },
+    { path: '**', redirectTo: 'landing', pathMatch: 'full' }
+];
