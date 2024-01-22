da057dd81f292bad6725e5572f1241c77bdc46ef
Sn00pyW00dst0ck <gabealdous@gmail.com>
2024-01-22 15:32:32 -0500

    Build out guard and service some

--- frontend/src/app/auth/oauth2.guard.ts
+++ frontend/src/app/auth/oauth2.guard.ts
@@ -1,17 +1,63 @@
 
-import { CanActivateFn } from "@angular/router";
+import { CanActivateFn, Router } from "@angular/router";
+import { inject } from "@angular/core";
 
+import { OAuth2Service } from "./oauth2.service";
 
-
+/**
+ * Guard to prevent access to the sign up page for authenticated users.
+ * Will redirect to 
+ */
 export const signupGuard: CanActivateFn = async () => {
+    const authService = inject(OAuth2Service);
+    const router = inject(Router);
+
+    const isUserAuthenticated = await authService.verify_token();
+
+    if (isUserAuthenticated) {
+        // Navigate somewhere
+        router.navigate(['/'])
+        return false;
+    }
+
     return true;
 };
 
+/**
+ * Guard to prevent access to the sign in page for authenticated users.
+ * Will redirect to 
+ */
 export const signinGuard: CanActivateFn = async () => {
+    const authService = inject(OAuth2Service);
+    const router = inject(Router);
+
+    const isUserAuthenticated = await authService.verify_token();
+
+    if (isUserAuthenticated) {
+        // Navigate somewhere
+        router.navigate(['/'])
+        return false;
+    }
+
     return true;
 };
 
+/**
+ * Guard to prevent access to the sign out page for unauthenticated users.
+ * Will redirect to 
+ */
 export const signoutGuard: CanActivateFn = async () => {
+    const authService = inject(OAuth2Service);
+    const router = inject(Router);
+
+    const isUserAuthenticated = await authService.verify_token();
+
+    if (!isUserAuthenticated) {
+        // Navigate somewhere
+        router.navigate(['/'])
+        return false;
+    }
+
     return true;
 };
 
--- frontend/src/app/auth/oauth2.service.ts
+++ frontend/src/app/auth/oauth2.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { toSignal } from '@angular/core';
 import { SignInInfo, SignUpInfo, SuccessfulUserAuth } from '../core/models/Auth';
+import { firstValueFrom } from 'rxjs';
 
 
 export class OAuth2Service {
@@ -8,25 +8,41 @@ export class OAuth2Service {
 
     constructor(private httpClient: HttpClient) { }
 
-    public sign_up(signUpInfo: SignUpInfo) {
-        const OBSERVABLE = this.httpClient.post<any>(this.REST_API_SERVER + "auth/sign_up", signUpInfo);
-        return toSignal(OBSERVABLE).asReadonly();
-        // make it automatically set tokens
-        // make it automatically handle errors
-        // make it a signal
+    public async sign_up(signUpInfo: SignUpInfo) {
+        try {
+            const res = await firstValueFrom(this.httpClient.post<any>(this.REST_API_SERVER + "auth/sign_up", signUpInfo));
+        } catch (error) {
+            throw error;
+        }
     }
 
-    public sign_in(signInInfo: SignInInfo) {
+    public async sign_in(signInInfo: SignInInfo) {
         //remove tokens? 
-
-        const OBSERVABLE = this.httpClient.post<SuccessfulUserAuth>(this.REST_API_SERVER + "auth/sign_in", signInInfo);
-        return toSignal(OBSERVABLE).asReadonly();
-        // make it automatically set tokens
-        // make it automatically handle errors
-        // make it a signal
+        try {
+            const res = await firstValueFrom(this.httpClient.post<SuccessfulUserAuth>(this.REST_API_SERVER + "auth/sign_in", signInInfo));
+            // use localstorage to store the access token and possibly refresh tokens
+        } catch (error) {
+            throw error;
+        }
     }
 
-    public sign_out(): void {
-        //remove tokens
+    public async sign_out(): Promise<void> {
+        // TODO: Implement this logic.
+        // Should this ping a sign out route? None is currently there. 
+        // use localstorage to remove the access token.
+        // navigate the router to the login or main landing page.
+    }
+
+    /**
+     * Verifies the user access token and that the user is logged in.
+     * 
+     */
+    public async verify_token(): Promise<boolean> {
+        try {
+            return true; // TODO: Implement this logic.
+        } catch (error) {
+            // Token is invalid or user is unauthorized.
+            return false;
+        }
     }
 }
