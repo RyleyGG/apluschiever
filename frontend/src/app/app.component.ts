2e4b22f1ef7a2ea5fe77dc2fd30a8f6adb050f1a
Sn00pyW00dst0ck <gabealdous@gmail.com>
2024-01-26 16:41:11 -0500

    PrimeNG setup, with dynamic theme swap on a button

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
 import { RouterOutlet } from '@angular/router';
 import { InternetConnectionService } from './core/services/internet-connection/internet-connection.service';
+import { ThemeService } from './core/services/theme/theme.service';
 
 /**
  * The main application component, currently the sample hello world page.
@@ -9,14 +11,23 @@ import { InternetConnectionService } from './core/services/internet-connection/i
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'apluschiever';
 
-  constructor(private internetConnection: InternetConnectionService) {
+  constructor(private themeService: ThemeService, private internetConnection: InternetConnectionService) {
     console.log(this.internetConnection.isOnline());
+  }
+
+  /**
+   * Sample method for testing. Shows how to use the theme service to read the theme, and update 
+   * the theme to a new one.
+   */
+  swapTheme(): void {
+    const newTheme = this.themeService.theme() == 'arya-blue' ? 'saga-blue' : 'arya-blue';
+    this.themeService.setTheme(newTheme);
   }
 }
