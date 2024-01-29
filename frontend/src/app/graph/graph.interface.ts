804332d7fc88c456b764a9d7c8b0577998bc02df
Sn00pyW00dst0ck <gabealdous@gmail.com>
2024-01-29 16:18:18 -0500

    Fill out basic interfaces for use.

--- frontend/src/app/graph/graph.interface.ts
+++ frontend/src/app/graph/graph.interface.ts
@@ -1,9 +1,27 @@
-export interface Node {
+export interface NodePosition {
+    x: number;
+    y: number;
+}
 
+export interface NodeDimension {
+    width: number;
+    height: number;
+}
+
+export interface Node {
+    id: string;
+    position?: NodePosition;
+    dimension?: NodeDimension;
+    transform?: string;
+    label?: string;
+    data?: any;
 };
 
 export interface Edge {
-
+    id?: string;
+    source: string;
+    target: string;
+    label?: string;
 };
 
 export interface Cluster {
