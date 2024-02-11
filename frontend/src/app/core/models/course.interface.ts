b91d511ef1048c67f0cfde9cd0f4d120f833d9f0
Ryley <39408998+RyleyGG@users.noreply.github.com>
2024-02-11 13:58:48 -0500

    update models

--- frontend/src/app/core/models/course.interface.ts
+++ frontend/src/app/core/models/course.interface.ts
@@ -1,19 +1,6 @@
 import {Markdown, Video} from "./node-content.interface";
 import {User} from "./user.interface";
 
-
-/**
- * Represents a content node within a course, including videos and markdown files.
- */
-export interface Node {
-    id: string;
-    title: string;
-    shortDescription: string;
-    videos?: Video[];
-    markdownFiles?: Markdown[];
-    courses: Course[];
-}
-
 /**
  * Represents a course, including its associated nodes and owner.
  */
