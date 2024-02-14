6f724e19bc3408153a3574c19f537847e6c7ab9d
kseniavelichko <kvelichko@ufl.edu>
2024-02-14 17:48:18 -0500

    test changes

--- frontend/src/app/pages/dashboard/dashboard.component.spec.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { DashboardComponent } from './dashboard.component';
 
 describe('DashboardComponent', () => {
@@ -8,7 +9,11 @@ describe('DashboardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DashboardComponent]
+      imports: [DashboardComponent],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting()
+    ]
     })
     .compileComponents();
     
