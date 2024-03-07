4aaf2f1481ccb6e36a35b75c5f13dcd52b7b75e6
kseniavelichko <kvelichko@ufl.edu>
2024-03-07 15:22:12 -0500

    added buttons to navigate

--- frontend/src/app/pages/lesson/lesson.component.ts
+++ frontend/src/app/pages/lesson/lesson.component.ts
@@ -3,11 +3,13 @@ import { ActivatedRoute } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DragDropModule } from 'primeng/dragdrop';
+import { ButtonModule } from 'primeng/button';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-lesson',
   standalone: true,
-  imports: [CommonModule, DragDropModule],
+  imports: [CommonModule, DragDropModule, ButtonModule],
   templateUrl: './lesson.component.html',
   styleUrl: './lesson.component.css'
 })
@@ -19,7 +21,10 @@ export class LessonComponent {
   dragStart(container: string) {
     console.log(container);
   }
-  constructor(private route : ActivatedRoute) {
+  constructor(private route : ActivatedRoute, private location: Location) {
   }
   title = this.route.snapshot.paramMap.get('id');
+  return(): void {
+    this.location.back();
+  }
 }
