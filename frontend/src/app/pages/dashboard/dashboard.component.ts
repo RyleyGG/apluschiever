ddc4034500e435f71c28c076dd1cd41545ec8f5e
Sn00pyW00dst0ck <gabealdous@gmail.com>
2024-03-18 17:01:59 -0400

    Started work on dashboard UI update

--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -4,7 +4,7 @@ import { SuccessfulUserAuth } from '../../core/models/auth.interface';
 import { CheckboxModule } from 'primeng/checkbox';
 import { FormBuilder, Validators } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
-import { FormsModule } from '@angular/forms'; 
+import { FormsModule } from '@angular/forms';
 import { CardModule } from 'primeng/card';
 import { Router } from '@angular/router';
 import { DataViewModule } from 'primeng/dataview';
@@ -12,9 +12,12 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { CourseService } from '../../core/services/course/course.service';
 import { UserService } from '../../core/services/user/user.service';
 import { CarouselModule } from 'primeng/carousel';
-import {Course} from "../../core/models/course.interface";
+import { Course } from "../../core/models/course.interface";
 import { ConfirmationService, MessageService } from 'primeng/api';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
+
+
+
 
 @Component({
   selector: 'app-dashboard',
@@ -29,24 +32,26 @@ export class DashboardComponent {
   userCourses: Course[] = [];
   responsiveOptions: any[] | undefined;
   showOptions = false;
+
+
   constructor(private courseService: CourseService, private userService: UserService, private confirmationService: ConfirmationService, private messageService: MessageService) {
     this.responsiveOptions = [
       {
-          breakpoint: '1199px',
-          numVisible: 1,
-          numScroll: 1
+        breakpoint: '1199px',
+        numVisible: 1,
+        numScroll: 1
       },
       {
-          breakpoint: '991px',
-          numVisible: 2,
-          numScroll: 1
+        breakpoint: '991px',
+        numVisible: 2,
+        numScroll: 1
       },
       {
-          breakpoint: '767px',
-          numVisible: 1,
-          numScroll: 1
+        breakpoint: '767px',
+        numVisible: 1,
+        numScroll: 1
       }
-  ];
+    ];
     this.courseService.getCourses().subscribe((data) => {
       this.allCourses = [];
       data.forEach((element: Course) => {
@@ -60,29 +65,33 @@ export class DashboardComponent {
       });
     });
   }
+
+
   addCourse(courseid: string) {
     this.userService.addCourse(courseid).subscribe((data) => {
       window.location.reload();
     });
   }
+
+
   confirm(courseid: string) {
-      this.confirmationService.confirm({
-        message: 'Are you sure that you want to proceed?',
-            header: 'Confirmation',
-            icon: 'pi pi-exclamation-triangle',
-            acceptIcon:"none",
-            rejectIcon:"none",
-            rejectButtonStyleClass:"p-button-text",
-            accept: () => {
-                this.messageService.add({ severity: 'info', summary: 'Confirmed', detail: 'You have accepted' });
-                this.userService.removeCourse(courseid).subscribe((data) => {
-                  window.location.reload();
-                })
-            },
-            reject: () => {
-                this.messageService.add({ severity: 'error', summary: 'Rejected', detail: 'You have rejected', life: 3000 });
-            }
-      });
+    this.confirmationService.confirm({
+      message: 'Are you sure that you want to proceed?',
+      header: 'Confirmation',
+      icon: 'pi pi-exclamation-triangle',
+      acceptIcon: "none",
+      rejectIcon: "none",
+      rejectButtonStyleClass: "p-button-text",
+      accept: () => {
+        this.messageService.add({ severity: 'info', summary: 'Confirmed', detail: 'You have accepted' });
+        this.userService.removeCourse(courseid).subscribe((data) => {
+          window.location.reload();
+        })
+      },
+      reject: () => {
+        this.messageService.add({ severity: 'error', summary: 'Rejected', detail: 'You have rejected', life: 3000 });
+      }
+    });
   }
-  
+
 }
