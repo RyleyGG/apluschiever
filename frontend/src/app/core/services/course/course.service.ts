dea0d9d3b2cde3d287863fc340b28f2eea1f16ac
RyleyGG <griffithryley2000@gmail.com>
2024-03-18 22:07:35 -0400

    integrate is_published field into dashboard

--- frontend/src/app/core/services/course/course.service.ts
+++ frontend/src/app/core/services/course/course.service.ts
@@ -1,65 +1,66 @@
-import { HttpClient, HttpErrorResponse } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { catchError, map, take, throwError } from "rxjs";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {Injectable} from "@angular/core";
+import {catchError, map, take, throwError} from "rxjs";
+import {CourseFilters} from "../../models/course.interface";
 
 /**
  * A service which interacts with the course endpoints.
  */
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class CourseService {
-    /**
-     * The server to hit
-     */
-    private REST_API_SERVER = "http://localhost:8000/";
+  /**
+   * The server to hit
+   */
+  private REST_API_SERVER = "http://localhost:8000/";
 
-    /**
-     * 
-     */
-    constructor(private httpClient: HttpClient) {
+  /**
+   *
+   */
+  constructor(private httpClient: HttpClient) {
 
-    }
+  }
 
-    // '/node/progress/{course_id}'
+  // '/node/progress/{course_id}'
 
-    // '/node/update_node' -> send node update in body
+  // '/node/update_node' -> send node update in body
 
-    /**
-     * Get all the nodes for a course
-     * @param course_id 
-     * @returns
-     */
-    getNodes(course_id: string) {
-        return this.httpClient.get<any>(this.REST_API_SERVER + `course/nodes/${course_id}`).pipe(
-            take(1),
-            map((res: any) => {
-                console.log(res);
-                return res;
-            }),
-            catchError((error: HttpErrorResponse) => {
-                // Something went really wrong
-                return throwError(() => error);
-            })
-        );
-    }
+  /**
+   * Get all the nodes for a course
+   * @param course_id
+   * @returns
+   */
+  getNodes(course_id: string) {
+    return this.httpClient.get<any>(this.REST_API_SERVER + `course/nodes/${course_id}`).pipe(
+      take(1),
+      map((res: any) => {
+        console.log(res);
+        return res;
+      }),
+      catchError((error: HttpErrorResponse) => {
+        // Something went really wrong
+        return throwError(() => error);
+      })
+    );
+  }
 
-    /**
-     * Get all courses
-     * @returns
-     */
-    getCourses() {
-        return this.httpClient.post<any>(this.REST_API_SERVER + `course/search`, {}).pipe(
-            take(1),
-            map((res: any) => {
-                console.log(res);
-                return res;
-            }),
-            catchError((error: HttpErrorResponse) => {
-                // Something went really wrong
-                return throwError(() => error);
-            })
-        );
-    }
+  /**
+   * Get all courses
+   * @returns
+   */
+  getCourses(courseFilters: CourseFilters = {}) {
+    return this.httpClient.post<any>(this.REST_API_SERVER + `course/search`, courseFilters).pipe(
+      take(1),
+      map((res: any) => {
+        console.log(res);
+        return res;
+      }),
+      catchError((error: HttpErrorResponse) => {
+        // Something went really wrong
+        return throwError(() => error);
+      })
+    );
+  }
 
 }
