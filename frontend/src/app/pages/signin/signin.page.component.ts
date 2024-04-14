91d18232fb793b43f22553ab0393cc86f4b79bc6
Sn00pyW00dst0ck <gabealdous@gmail.com>
2024-04-14 10:11:03 -0400

    Updated Sign In Page UI

--- frontend/src/app/pages/signin/signin.page.component.ts
+++ frontend/src/app/pages/signin/signin.page.component.ts
@@ -9,8 +9,11 @@ import { ButtonModule } from 'primeng/button';
 import { SignInInfo } from '../../core/models/auth.interface';
 import { FormsModule } from '@angular/forms';
 import { CardModule } from 'primeng/card';
+import { MessagesModule } from 'primeng/messages';
+import { MessageModule } from 'primeng/message';
+import { InputTextModule } from 'primeng/inputtext';
 import { ReactiveFormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { of } from 'rxjs';
 
 /**
@@ -21,39 +24,41 @@ import { of } from 'rxjs';
 @Component({
     selector: 'signin-page',
     standalone: true,
-    imports: [CommonModule, FormsModule, CardModule, ReactiveFormsModule, ButtonModule, CheckboxModule],
+    imports: [CommonModule, FormsModule, InputTextModule, MessagesModule, MessageModule, CardModule, ReactiveFormsModule, ButtonModule, CheckboxModule, RouterModule],
     templateUrl: './signin.page.component.html',
     styleUrl: './signin.page.component.css'
 })
 export class SignInPageComponent {
-    errorMessage = ""
+    errorMessage = "";
+
     signinForm = this.fb.group({
         email_address: ['', [Validators.required, Validators.email]],
         password: ['', Validators.required]
-    })
-    title = 'Sign-In Page'
+    });
 
     constructor(private router: Router, private fb: FormBuilder, private oauthService: OAuth2Service) { }
 
     get email_address() {
         return this.signinForm.controls['email_address'];
     }
+
     get password() { return this.signinForm.controls['password']; }
 
+    /**
+     * Signs in the user using the login form information.
+     */
     public signin(): void {
         const data = {
             username: this.signinForm.value.email_address,
             password: this.signinForm.value.password
         };
+
         this.oauthService.sign_in(data as SignInInfo).subscribe((res: SuccessfulUserAuth) => {
-            console.log(res);
             this.router.navigate(['/dashboard']);
             window.location.reload();
-        },
-        (error) => {
+        }, (error) => {
             //errors to account for -> wrong email or password
             this.errorMessage = 'Please check that your email and password are correct.';
-          }
-        );
+        });
     }
 }
