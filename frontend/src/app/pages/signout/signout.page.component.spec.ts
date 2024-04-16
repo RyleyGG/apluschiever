0c9b09437c035f73ba8d1fe7a0125badadd5807a
kseniavelichko <kvelichko@ufl.edu>
2024-04-15 22:43:09 -0400

    fixed testing for frontend

--- frontend/src/app/pages/signout/signout.page.component.spec.ts
+++ frontend/src/app/pages/signout/signout.page.component.spec.ts
@@ -3,6 +3,8 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 import { SignOutPageComponent } from './signout.page.component';
+import { routes } from '../../app.routes';
+import { provideRouter } from '@angular/router';
 
 describe('SignOutPageComponent', () => {
     let component: SignOutPageComponent;
@@ -12,7 +14,8 @@ describe('SignOutPageComponent', () => {
             imports: [SignOutPageComponent],
             providers: [
                 provideHttpClient(),
-                provideHttpClientTesting()
+                provideHttpClientTesting(),
+                provideRouter(routes)
             ]
         }).compileComponents();
         component = TestBed.createComponent(SignOutPageComponent).componentInstance;
