e617bf4f96baa9abe57430bc264d9deb614f5d15
kseniavelichko <kvelichko@ufl.edu>
2024-04-15 22:29:35 -0400

    improved searching to work with filters

--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -80,8 +80,11 @@ export class DashboardComponent {
   public updatedFirstName: string = "";
   public updatedLastName: string = "";
   public updatedEmail: string = "";
-
-
+  public searchedCourses: any[] = [];
+  ownedByMe: boolean = false;
+  all: boolean = true;
+  completed: boolean = false;
+  inProgress: boolean = false;
   constructor(private courseService: CourseService, private userService: UserService, private confirmationService: ConfirmationService, private messageService: MessageService) {
     // Get the courses the user is enrolled in...
     this.userService.getUserCourses().subscribe((data) => {
@@ -109,6 +112,7 @@ export class DashboardComponent {
       this.userInProgressCourses = this.userCourses.filter((course) => course.progress! !== 100);
 
       this.displayedCourses = this.userCourses;
+      this.searchedCourses = this.userCourses;
     });
 
     // Get user information and the coureses the user owns...
@@ -144,9 +148,41 @@ export class DashboardComponent {
     });
   }
   search(value: string) {
-    this.displayedCourses = this.userCourses.filter(course =>
+    this.searchedCourses = this.displayedCourses.filter(course =>
       course.title.toLowerCase().includes(value.toLowerCase())
     );
+  }
+  displayAll() {
+    this.displayedCourses = this.userCourses;
+    this.search(this.searchValue);
+    this.all = true;
+    this.inProgress = false;
+    this.completed = false;
+    this.ownedByMe = false;
+  }
+  displayInProgress() {
+    this.displayedCourses = this.userInProgressCourses;
+    this.search(this.searchValue);
+    this.all = false;
+    this.inProgress = true;
+    this.completed = false;
+    this.ownedByMe = false;
+  }
+  displayComplete() {
+    this.displayedCourses = this.userCompletedCourses;
+    this.search(this.searchValue);
+    this.all = false;
+    this.inProgress = false;
+    this.completed = true;
+    this.ownedByMe = false;
+  }
+  displayOwned() {
+    this.displayedCourses = this.teachingCourses;
+    this.search(this.searchValue);
+    this.all = false;
+    this.inProgress = false;
+    this.completed = false;
+    this.ownedByMe = true;
   }
   /**
    * Controls the confirmation pop-up when unenrolling from a course
