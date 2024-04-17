b786c7f6df01c97f64f4617f5936a81aa3cb7878
Sn00pyW00dst0ck <gabealdous@gmail.com>
2024-04-17 13:54:14 -0400

    Fix a route and spacing bug

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -29,5 +29,5 @@ export const routes: Routes = [
     // General
     { path: "landing", component: LandingPageComponent },
     { path: '', redirectTo: '/landing', pathMatch: 'full' },
-    { path: '**', component: AppComponent } // TODO: make this a PageNotFound or 404 error page
+    { path: '**', redirectTo: '/landing' } // TODO: make this a PageNotFound or 404 error page
 ];
